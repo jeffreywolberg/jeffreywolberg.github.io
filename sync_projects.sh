39e6beefd926775f096fde413c2ba9c45295f25a
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2025-07-07 02:25:02 -0400

    direct youtube page scrape frontend to pisces backend

--- sync_projects.sh
+++ sync_projects.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-SRC="/Users/jeffreywolberg/Coding/youtube_page_scrape/index.html"
-DEST="/Users/jeffreywolberg/Coding/jeffreywolberg.github.io/projects/youtube_page_scrape/"
+OS_TYPE=$(uname)
 
-rsync -av --exclude='.git' --delete "$SRC" "$DEST"
+if [ "$OS_TYPE" = "Linux" ]; then
+# pisces machine at home
+	echo "Running on Linux"
+	SRC="/home/jeffrey/Coding/youtube_page_scrape/index.html"
+	DEST="/home/jeffrey/Coding/jeffreywolberg.github.io/projects/youtube_page_scrape/"
+elif [ "$OS_TYPE" = "Darwin" ]; then
+# My local mac
+	echo "Running on Mac"
+	SRC="/Users/jeffreywolberg/Coding/youtube_page_scrape/index.html"
+	DEST="/Users/jeffreywolberg/Coding/jeffreywolberg.github.io/projects/youtube_page_scrape/"
+else
+	echo "Unknown OS: $OS_TYPE"
+fi
+
+mkdir -p "$DEST"
+rsync -av --checksum --exclude='.git' --delete "$SRC" "$DEST"
